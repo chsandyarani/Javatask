86fc06f37030c103a72acb6e704b907ae12a7ae2
Sandyarani Chinapuram <inschin@KMC.Corp>
2022-05-04 10:55:16 +0530

    commit

--- DailyTask/src/main/java/Programs/AscendingOrder.java
+++ DailyTask/src/main/java/Programs/AscendingOrder.java
@@ -3,44 +3,29 @@ package Programs;
 import java.util.Arrays;
 
 public class AscendingOrder {
-    public static void main(String[] args) {
-        int[] arr = new int[] {4,3,5,6,1,2};
+
+    public static  void ascending(int[] array,boolean flag)
+    {
         int temp;
-        Boolean flag;
-        if (flag = false)
-        {
-            for (int i = 0; i < arr.length; i++)
-            {
-                for (int j = i + 1; j < arr.length; j++)
-                {
-                    if (arr[i] > arr[j])
-                    {
-                        temp = arr[i];
-                        arr[i] =arr[j];
-                        arr[j] = temp;
+        if (flag ) {
+            for (int i = 0; i < array.length; i++) {
+                for (int j = i + 1; j < array.length; j++) {
+                    if (array[i] > array[j]) {
+                        temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
                     }
                 }
 
             }
-            System.out.println("Ascending order:" + Arrays.toString(arr));
+            System.out.println("Ascending order:" + Arrays.toString(array));
         }
-        else
-        {
-            for (int i = 0; i < arr.length; i++)
-            {
-                for (int j = i + 1; j < arr.length; j++)
-                {
-                    if (arr[i] < arr[j])
-                    {
-                        temp = arr[i];
-                        arr[i] = arr[j];
-                        arr[j] = temp;
+    }
 
-                    }
-                }
-            }
-            System.out.println("Descending order:" + Arrays.toString(arr));
-        }
+    public static void main(String[] args) {
+        int[] array = {4, 3, 5, 6, 1, 2};
+        boolean b = true;
+        ascending( array ,b);
+    }
 
     }
-}
